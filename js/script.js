4ba30b01ebd668ca7b3fbea2322d13eb10c82f5d
Ahmad Yousef <dawabsheh@engineer.com>
2021-06-22 21:28:41 +0300

    edited the project to fit requirements

--- js/script.js
+++ js/script.js
@@ -1,23 +1,37 @@
 'use-strict'
 
-var name = prompt("Hello there, please enter your name so start the little journey on my website ☺");
-var q1 = prompt("Welcome " + name + ", Are you above 18 years old ? (answer y/n)");
+let name = prompt("Hello there, please enter your name so start the little journey on my website ☺");
+let q1 = prompt("Welcome " + name + ", do you agree that my name is 'Ahmad' ? (answer y/n)").toLocaleLowerCase();
+let q2 = prompt("do you agree that I am from Jordan ? (answer y/n)").toLocaleLowerCase();
+let q3 = prompt("do you agree that I am 15 years old ? (answer y/n)").toLocaleLowerCase();
+let q4 = prompt("do you agree that I graduated from BAU in Journalism ? (answer y/n)").toLocaleLowerCase();
+let q5 = prompt("do you agree that I am a full stack php developer ? (answer y/n)").toLocaleLowerCase();
 
-q1 = q1.toLowerCase();
+let score = 0;
 
- 
+socre = parseInt(score);
+
+
+console.log(q1);
 while(["y", "n"].indexOf(q1) == -1){
    q1 = prompt("Wrong answer ! please enter 'y' or 'n'");
 }
 
-if(q1 == "y"){
-    // console.log("Great ! you are above 18 years old");
-    alert("Great ! you are above 18 years old");
-} else if(q1 == "n"){
-    // console.log("Telling lies " + name + "? I'm sure you are above 18 because you are seeing this from LTUC, Loool ☺");
-    alert("Telling lies " + name + "? I'm sure you are above 18 because you are seeing this from LTUC, Loool ☺");
-}
+if(q1 == 'y'){score = score+1;}
+console.log(socre);
 
-var age = prompt("Welcome " + name + ", please enter your age to calculate how many days you have lived !");
 
-alert ('You have lived about ' + (age*365) + " days in your life, I hope you a long happy life " + name + " ♥");
+ if(q2 == 'y'){score = score+1;}
+
+ if(q3 == 'n'){score = score+1;}
+
+
+ if(q4 == 'n'){score = score+1;}
+
+ 
+ if(q5 == 'y'){score = score+1;}
+
+
+
+
+alert ('You have answered ' + score + ' right answers, wich means your score is' + score*20 + "/100");
